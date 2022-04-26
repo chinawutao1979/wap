3757dbc8abb217f221f02c7847fb0f10322802a6
chinawutao1979 <103130005+chinawutao1979@users.noreply.github.com>
2022-04-26 05:00:33 -0500

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+if(true){
+    some=screen;
+    if(true){
+
+    }else{
+        if(true){
+            function somethiing(){
+                Array.array.forEach(element => {
+                    
+                });
+            }
+        }
+    }
+}
+// comment
+// ? asdf
+// ! dasfa
+// todo
+
